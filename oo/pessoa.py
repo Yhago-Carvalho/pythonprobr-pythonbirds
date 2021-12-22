60e3e65e5bbbcf7250ba133e9c348688d3aac0a2
Yhago Carvalho <yhagocarvalho94@gmail.com>
2021-12-22 10:09:58 -0300

    Criando classe
    Criou-se uma classe, atribuiu-se atributos, removeu e adicionou atributos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,23 @@
 class Pessoa():
-    pass
+    def __init__(self, *filhos, nome=None, idade=27):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return 'Olá'
+
+
+if __name__ == '__main__':
+    jorge = Pessoa(nome='Jorge', idade=25)
+    joão = Pessoa(jorge, nome='joão', idade=50)
+    print(joão.cumprimentar())
+    print(joão.nome)
+    print(joão.idade)
+    for filho in joão.filhos:
+        print(filho.nome)
+    joão.sobrenome = 'Carvalho'
+    del joão.filhos
+    print(joão.__dict__)
+    print(jorge.__dict__)
+    
